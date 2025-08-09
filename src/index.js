a352009c70226fabdb30a476a29c7a1ee97ec698
F41lo62 <inopredictautoplay@gmail.com>
2025-08-09 12:58:01 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ console.log("🚀 INO-Predict Auto Express v2 iniciado com sucesso!");
 
 const { calcularMelhorMomento } = require("./services/previsoes");
 
-console.log("Sugestão de melhor momento:", const express = require("express");
+const express = require("express");
 const app = express();
 
 const port = process.env.PORT || 3000;
+
+console.log("Sugestão de melhor momento:", calcularMelhorMomento());
 
 app.get("/", (req, res) => {
   res.send("INO-Predict Auto Express v2 está rodando!");
@@ -13,4 +15,4 @@ app.get("/", (req, res) => {
 
 app.listen(port, () => {
   console.log(`Servidor rodando na porta ${port}`);
-});calcularMelhorMomento());
+});
